a6c35a6f8eb15eebebaf5f809dfb99dbf10a7010
Hanna Angeria <haan2402@student.miun.se>
2025-05-13 14:07:09 +0200

    lagt till komponenter jag behöver och gjort routes och en 404-sida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CoursesComponent } from './courses/courses.component';
+import { ScheduleComponent } from './schedule/schedule.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'courses', component: CoursesComponent },
+    { path: 'schedule', component: ScheduleComponent},
+    { path: '', redirectTo: '/courses', pathMatch: 'full'}
+];
