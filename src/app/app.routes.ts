06a5b55d5de2246f5099d10477f64521cdc89722
Hanna Angeria <haan2402@student.miun.se>
2025-05-13 14:09:36 +0200

    här la jag till en 404-sida och routing för det

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { CoursesComponent } from './courses/courses.component';
 import { ScheduleComponent } from './schedule/schedule.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
     { path: 'courses', component: CoursesComponent },
     { path: 'schedule', component: ScheduleComponent},
-    { path: '', redirectTo: '/courses', pathMatch: 'full'}
+    { path: '', redirectTo: '/courses', pathMatch: 'full'},
+    { path: '404', component: NotFoundComponent },
+    { path: '**', component: NotFoundComponent}
 ];
